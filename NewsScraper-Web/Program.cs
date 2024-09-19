9fe705c3be0da1ddd07f0399719e7a7354b626d4
jdlandau123 <jdlandau123@gmail.com>
2024-09-18 22:03:33 -0400

    use https

--- NewsScraper-Web/Program.cs
+++ NewsScraper-Web/Program.cs
@@ -46,6 +46,7 @@ public class Program
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
+            app.UseHttpsRedirection();
         }
 
         app.UseHttpsRedirection();
